95e2bab0a9a6d79c83250085b88d011b5d2c8fb3
Tai Lucas <tglucas@gmail.com>
2023-01-25 08:06:55 +0200

    Add option to install wheel (on newest pip/setuptools).

--- app_setup.sh
+++ app_setup.sh
@@ -6,9 +6,11 @@ set -o pipefail
 # output site configuration
 python -m site
 export PIP_DEFAULT_TIMEOUT=60
-#python -m pip install --upgrade pip
-#python -m pip install --upgrade setuptools
-#python -m pip install --upgrade wheel
+if [ -n "${PYTHON_ADD_WHEEL:-}" ]; then
+  python -m pip install --upgrade pip
+  python -m pip install --upgrade setuptools
+  python -m pip install --upgrade wheel
+fi
 python -m pip install -r "/opt/app/requirements.txt"
 
 deactivate
