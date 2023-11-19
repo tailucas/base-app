3c895b793ef1236f8d8da625f097281f0b6af9a7
Tai Lucas <tglucas@gmail.com>
2023-11-19 09:13:34 +0200

    Simplify base-app and Java dependencies.

--- src/main/java/tailucas/app/App.java
+++ src/main/java/tailucas/app/App.java
@@ -9,13 +9,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.zeromq.ZMQ;
-
-import org.zeromq.ZContext;
-import org.zeromq.SocketType;
-
 import org.ini4j.Ini;
-
 
 public class App 
 {
@@ -37,56 +31,26 @@ public class App
 
     public static void main( String[] args )
     {
-        final Locale locale = Locale.getDefault();
-        log.info("Locale language: {} ", locale.getLanguage());
-        log.info("Locale country: {}", locale.getCountry());
         Thread.currentThread().setName("main");
         registerShutdownHook();
+        final Locale locale = Locale.getDefault();
+        log.info("Locale: {} {}", locale.getLanguage(), locale.getCountry());
         final Map<String, String> envVars = System.getenv();
-        log.info("Starting application with env {}", envVars.keySet());
-
-        /*
-        OnePassword op = new OnePassword();
-        op.getItems();
-        */
-
-        final String javaVersion = Runtime.version().toString();
-        ZContext context = new ZContext();
-        ZMQ.Socket socket = context.createSocket(SocketType.PUSH);
-        log.info( "Hello (print) " + javaVersion );
-        log.trace("Hello (trace) {} ", javaVersion);
-        log.debug("Hello (debug) {} ", javaVersion);
-        log.info("Hello (info) {} ", javaVersion);
-        log.error("Hello? (error) {}", javaVersion);
-        socket.close();
-        context.close();
+        log.info("Environment variable keys: {}", envVars.keySet());
+        log.info( "Java runtime: " + Runtime.version().toString() );
         Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
         for (Thread thread : threadSet) {
-            log.info(thread + " daemon? " + thread.isDaemon());
+            log.info("{} daemon? {}", thread.getName(), thread.isDaemon());
         }
-        log.info("Working directory is: " + System.getProperty("user.dir"));
+        log.info("Working directory: " + System.getProperty("user.dir"));
         try {
             Ini appConfig = new Ini(new File("./app.conf"));
             log.info("App Device Name: " + appConfig.get("app", "device_name"));
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
-
-        /*
-        log.info("Starting MQTT client...");
-        Mqtt mqtt = new Mqtt();
-        mqtt.start();
-
-        log.info("Starting Rabbit MQ client...");
-        RabbitMq rabbit = new RabbitMq();
-        rabbit.start();
-
-        MyClass myc = new MyClass("foo");
-        myc.getAge();
-        */
-
         try {
-            Thread.currentThread().sleep(2000);
+            Thread.sleep(2*1000);
         } catch (InterruptedException e) {
             log.error(e.getMessage(), e);
         }
