da0a54d455a399b2fdb8d0e1e39992af39756b73
Tai Lucas <tglucas@gmail.com>
2024-10-19 08:17:03 +0200

    Make cron process optional.

--- base_entrypoint.sh
+++ base_entrypoint.sh
@@ -4,13 +4,18 @@ set -o pipefail
 
 /opt/app/config_interpol < /opt/app/config/app.conf > /opt/app/app.conf
 cp /opt/app/config/supervisord.conf /opt/app/supervisord.conf
-# cron
-cat << EOF >> /opt/app/supervisord.conf
+
+if [ "${NO_CRON:-}" != "true" ]; then
+  # cron
+  cat << EOF >> /opt/app/supervisord.conf
 [program:cron]
 command=/usr/sbin/cron -f -L 4
 autorestart=unexpected
 EOF
+fi
 printenv | sed 's/=\(.*\)/="\1"/' >> /opt/app/cron.env
+
+# set AWS config
 if [ -n "${AWS_DEFAULT_REGION:-}" ]; then
   # AWS configuration (no tee for secrets)
   /opt/app/config_interpol < /opt/app/config/aws-config > /home/app/.aws/config
