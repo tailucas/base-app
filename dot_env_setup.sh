3d87540275a4c3cad870dbc3c64523e0de201ee9
Tai Lucas <tglucas@gmail.com>
2025-09-18 21:57:44 +0200

    Output both modes of .env generation.

--- dot_env_setup.sh
+++ dot_env_setup.sh
@@ -6,10 +6,10 @@ PROJECT_NAME="${1}"
 DOT_ENV_FILE_PATH="${DOT_ENV_FILE_PATH:-.env}"
 
 if [ -z "${GITHUB_WORKFLOW_REF:-}" ]; then
-  rm -f "${DOT_ENV_FILE_PATH}"
+  echo "Generating "${DOT_ENV_FILE_PATH}" from pylib cred_tool."
   uv run cred_tool "ENV.${PROJECT_NAME}" "${PROJECT_NAME}" build | jq -r '. | to_entries[] | [.key,.value] | @tsv' | tr '\t' '=' | sed 's/=\(.*\)/="\1"/' > "${DOT_ENV_FILE_PATH}"
 else
-  echo "Generating "${DOT_ENV_FILE_PATH}" from Taskfile application definition due to CI workflow ${GITHUB_WORKFLOW_REF:-}"
+  echo "Generating "${DOT_ENV_FILE_PATH}" from Taskfile application definition due to CI workflow ${GITHUB_WORKFLOW_REF:-}..."
   uv run python -c "import yaml; data=yaml.safe_load(open('app.yml'))['vars']; [print(f'{k}={v}') for k,v in data.items()]" > "${DOT_ENV_FILE_PATH}"
 fi
 test $(wc -l < "${DOT_ENV_FILE_PATH}") -ge 2
