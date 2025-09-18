c14442b0e6d20ce637d130d679045d4463b99d99
Tai Lucas <tglucas@gmail.com>
2025-09-18 22:12:40 +0200

    Test another GH variable to detect CI.

--- dot_env_setup.sh
+++ dot_env_setup.sh
@@ -5,11 +5,11 @@ set -o pipefail
 PROJECT_NAME="${1}"
 DOT_ENV_FILE_PATH="${DOT_ENV_FILE_PATH:-.env}"
 
-if [ -z "${GITHUB_WORKFLOW_REF:-}" ]; then
+if [ -z "${GITHUB_ENV:-}" ]; then
   echo "Generating "${DOT_ENV_FILE_PATH}" from pylib cred_tool."
   uv run cred_tool "ENV.${PROJECT_NAME}" "${PROJECT_NAME}" build | jq -r '. | to_entries[] | [.key,.value] | @tsv' | tr '\t' '=' | sed 's/=\(.*\)/="\1"/' > "${DOT_ENV_FILE_PATH}"
 else
-  echo "Generating "${DOT_ENV_FILE_PATH}" from Taskfile application definition due to CI workflow ${GITHUB_WORKFLOW_REF:-}..."
+  echo "Generating "${DOT_ENV_FILE_PATH}" from Taskfile application definition due to CI ${GITHUB_ENV:-}..."
   uv run python -c "import yaml; data=yaml.safe_load(open('app.yml'))['vars']; [print(f'{k}={v}') for k,v in data.items()]" > "${DOT_ENV_FILE_PATH}"
 fi
 test $(wc -l < "${DOT_ENV_FILE_PATH}") -ge 2
