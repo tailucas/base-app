840dbe4bccc751d46924f0c8c66d875c92e55821
Tai Lucas <tglucas@gmail.com>
2026-01-08 09:24:24 +0200

    Clean .rustup to work around broken upgrade behavior.

--- rust_setup.sh
+++ rust_setup.sh
@@ -2,4 +2,6 @@
 set -eu
 set -o pipefail
 
+# Workaround for https://github.com/rust-lang/rustup/issues/1239
+rm -rf "${HOME}/.rustup/"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
